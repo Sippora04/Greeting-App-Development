f3a1cd4cb246bfe216ad464bf51b5db67ef560cc
Sippora04 <sippora.toppo95@gmail.com>
2020-12-20 19:34:54 +0530

    Added: using greeting controller to use services layer to get a message

--- main/java/com/example/greetingapp/controller/GreetingController.java
+++ main/java/com/example/greetingapp/controller/GreetingController.java
@@ -2,17 +2,33 @@ package com.example.greetingapp.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.greetingapp.model.Greeting;
+import com.example.greetingapp.service.IGreetingService;
 
+/*@RequestParam for accessing HTTP
+* request query parameters. 
+*/
+@RequestMapping("/greeting")
 public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
-	@GetMapping("/greeting")
+	/* @Autowired
+	 * This annotation is applied to fields, setter methods, and constructors.
+	 * The @Autowired annotation injects object dependency implicitly.
+	 */
+	
+	@Autowired
+	private IGreetingService greetingService;
+
+	@GetMapping("/get/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
+
 }
---
+++ main/java/com/example/greetingapp/service/GreetingService.java
@@ -0,0 +1,13 @@
+package com.example.greetingapp.service;
+
+import com.example.greetingapp.model.Greeting;
+import com.example.greetingapp.model.User;
+
+public class GreetingService {
+	
+	public String greet() {
+		return "Hello World";
+		
+	}
+
+}
